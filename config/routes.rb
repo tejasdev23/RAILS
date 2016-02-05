a024227c0875771d14d708d71cdffad0095d06ad
tejasdev23 <tejasdev23@gmail.com>
2016-02-05 22:10:28 +0530

    root path changed to courses#index

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'greeter/hello'
     get 'greeter/goodbye' 
     get '/courses/index/:looking_for' => 'courses#index'
+    root 'courses#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
